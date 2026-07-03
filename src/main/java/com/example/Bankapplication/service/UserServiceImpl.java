789fa6bf036a52f425db14471b690d3fbee25f22
Aarthi0905 <aarthideenadhayalan.498gmail.com>
2026-07-03 22:54:02 +0530

    Started user management module

---
+++ src/main/java/com/example/Bankapplication/service/UserServiceImpl.java
@@ -0,0 +1,37 @@
+package com.example.Bankapplication.service;
+
+import com.example.Bankapplication.entity.User;
+import com.example.Bankapplication.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public User registerUser(User user) {
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        return userRepository.save(user);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+}
