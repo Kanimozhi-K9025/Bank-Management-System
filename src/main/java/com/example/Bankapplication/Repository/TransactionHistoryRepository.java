02e5757d8835febdcc50f82abb79dca0de4aab74
Kanimozhi-K9025 <kkanimozhi9025@gmail.com>
2026-07-15 21:49:27 +0530

    Resolved merge conflict

--- src/main/java/com/example/Bankapplication/Repository/TransactionHistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Bankapplication.Repository;
-
-import com.example.Bankapplication.Entity.TransactionHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionHistoryRepository extends JpaRepository<TransactionHistory, Long> {
-}
--- src/main/java/com/example/Bankapplication/repository/FundTransferRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Bankapplication.repository;
-
-import com.example.Bankapplication.model.FundTransfer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FundTransferRepository
-        extends JpaRepository<FundTransfer, Long> {
-}
