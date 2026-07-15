7a7224b850cda308c77fdc25986300575bd029b6
Kanimozhi-K9025 <kkanimozhi9025@gmail.com>
2026-07-16 00:20:44 +0530

    restored all files

---
+++ src/main/java/com/example/Bankapplication/Service/AccountService.java
@@ -0,0 +1,41 @@
+package com.example.Bankapplication.Service;
+
+import com.example.Bankapplication.Entity.Account;
+import com.example.Bankapplication.Repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    // Create Account
+    public Account saveAccount(Account account) {
+        return accountRepository.save(account);
+    }
+
+    // Get All Accounts
+    public List<Account> getAllAccounts() {
+        return accountRepository.findAll();
+    }
+
+    // Get Account By ID
+    public Optional<Account> getAccountById(Long accountId) {
+        return accountRepository.findById(accountId);
+    }
+    //update Account
+    public Account updateAccount(Long accountId, Account account) {
+        account.setAccountId(accountId);
+        return accountRepository.save(account);
+    }
+
+    // Delete Account
+    public void deleteAccount(Long accountId) {
+        accountRepository.deleteById(accountId);
+    }
+}
